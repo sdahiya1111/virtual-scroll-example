9187a0d7e04240883aeb6715d02d564938b1e1c1
sdahiya1111 <shubhamdahiya1111@gmail.com>
2024-04-26 03:01:30 +0530

    add virtual scroll using transform translate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useMemo, useState } from 'react';
 import './App.css';
 import Card from './Card';
 import movies from './movies.json';
 
+const listCount = 10;
+const cardHeight = 152;
+
 function App() {
+  const moviesCount = useMemo(() => movies.length, []);
+
+  const [startIdx, setStartIdx] = useState(0);
+  const [padTop, setPadTop] = useState(0);
+
+  const scrollHandler = (e) => {
+    let newStartIdx = Number.parseInt(e.target.scrollTop / cardHeight);
+    newStartIdx = Math.min(newStartIdx, moviesCount - listCount);
+    setStartIdx(newStartIdx);
+    setPadTop(newStartIdx * cardHeight);
+  }
+
+  const moviesSlice = useMemo(() => movies.slice(startIdx, listCount + startIdx), [startIdx]);
 
   return (
     <div className="App">
@@ -11,9 +28,15 @@ function App() {
           Virtual Scrolling example
         </h1>
       </header>
-      <main>
-        <div role="list" className="card-list">
-          {movies.map(({ name, genre }, idx) => (<Card key={idx} name={name} genre={genre} />))}
+      <main onScroll={scrollHandler}>
+        <div
+          role="list"
+          className="card-list"
+          style={{ height: cardHeight * moviesCount }}
+        >
+          <div className="list-window" style={{transform: `translateY(${padTop}px)`}}>
+            {moviesSlice.map(({ name, genre }, idx) => (<Card key={idx} name={name} genre={genre} />))}
+          </div>
         </div>
       </main>
     </div>
